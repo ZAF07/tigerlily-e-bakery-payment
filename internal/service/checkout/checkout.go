b089634bfff473eac08663c59fd1f88c0fa41c06
ZAF07 <zaffere07@gmail.com>
2022-05-15 18:02:56 +0800

    temp commit debugging grpc error

--- internal/service/checkout/checkout.go
+++ internal/service/checkout/checkout.go
@@ -29,7 +29,35 @@ func NewCheckoutService(DB *gorm.DB) *Service {
 }
 
 	func (srv Service) Checkout(ctx context.Context, req *rpc.CheckoutReq) (resp *rpc.CheckoutResp, err error) {
-		// Remove this
+			statusURL := ""
+
+		// TO HANDLE PAYMENT STATUS RESPONSE (RETURN THE STATUS URL BACK TO THE CLIENT)
+		statusURL, err = stripe.CreateCheckoutSession() 
+		if err != nil {
+			resp = &rpc.CheckoutResp{
+				Success: false,
+				StatusUrl: statusURL,
+			}
+			return
+		}
+
+
+	checkoutSuccess, err := srv.base.CreateNewOrder(req.CheckoutItems)
+	if err != nil {
+		srv.logs.ErrorLogger.Printf("[SERVICE] Error processing database transaction: %+v\n", err)
+		srv.logs.ErrorLogger.Printf(" [SERVICE] RESULT FROM DS : %+v", checkoutSuccess)
+	}
+
+	srv.logs.InfoLogger.Printf(" [SERVICE] CREATE NEW ORDER STATUS : %+v\n",checkoutSuccess)
+
+	// USE ENUM AS ERROR CODES
+	resp = &rpc.CheckoutResp{
+		Success: checkoutSuccess,
+		StatusUrl: statusURL,
+		Message: "",
+	}
+
+	return
 		return nil, err
 	}
 
