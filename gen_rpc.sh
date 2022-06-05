8bbbd00898b310a4cba82b4c4ff185607ed80dc1
ZAF07 <zaffere07@gmail.com>
2022-06-05 19:15:46 +0800

    updating rpc proto..

--- gen_rpc.sh
+++ gen_rpc.sh
@@ -1,10 +1,15 @@
 echo 'export GOPATH=$HOME/Go' >> $HOME/.bashrc source $HOME/.bashrc
 
+rm -rf api/rpc/*
+
 # protoc --go_out=. --go_opt=paths=source_relative ./protos/*
 protoc --go_out=api/rpc --go_opt=paths=source_relative --go-grpc_out=api/rpc  --go-grpc_opt=paths=source_relative ./proto/payment/*
 
 echo "Copying proto files into api/rpc"
 cp -R api/rpc/proto/payment/* api/rpc
+
+echo "Comiting to capture changes.."
+git commit -am 'updating rpc proto..'
 
 echo "Removing unused directories"
 rm -rf api/rpc/proto
